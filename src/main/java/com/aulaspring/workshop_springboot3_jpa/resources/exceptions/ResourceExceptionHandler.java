d60be52b03bc9081b1254666f245c2695e170a03
Milton Bueno <miltonjunior17@gmail.com>
2025-10-17 19:42:29 -0300

    Exception handling - findById

---
+++ src/main/java/com/aulaspring/workshop_springboot3_jpa/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.aulaspring.workshop_springboot3_jpa.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.aulaspring.workshop_springboot3_jpa.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice //essa anotação é responsável por interceptar as excessões 
+//que acontecerem para que este objeto possa realizar um possível tratamento
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
