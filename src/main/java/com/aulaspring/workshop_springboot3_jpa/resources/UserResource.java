3b60a4f965eb8900600b8eea8efb6d1258888737
Milton Bueno <miltonjunior17@gmail.com>
2025-10-17 18:31:20 -0300

    User insert

--- src/main/java/com/aulaspring/workshop_springboot3_jpa/resources/UserResource.java
+++ src/main/java/com/aulaspring/workshop_springboot3_jpa/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.aulaspring.workshop_springboot3_jpa.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aulaspring.workshop_springboot3_jpa.entities.User;
 import com.aulaspring.workshop_springboot3_jpa.services.UserService;
@@ -28,9 +32,16 @@ public class UserResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id){
-		User obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user){
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
 
 }
